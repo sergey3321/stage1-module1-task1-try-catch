d15cfc12f6f1ea8125aebb605ad46485efbabca9
sergey3321 <mail.sergey21@gmail.com>
2023-04-06 19:22:12 +0300

    stage1-module1-task1-try-catch

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -1,9 +1,11 @@
 package com.epam.m1.exceptions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.lang.*;
+import java.util.stream.Collectors;
 
 /**
  * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
@@ -19,7 +21,6 @@ public class ParseIntegers {
         Iterator<String> words = WORDS.iterator();
         int sum = 0;
         String justWords = "";
-        String a [] = new String[0];
         while (words.hasNext()) {
             String next = words.next();
             try {
@@ -34,6 +35,11 @@ public class ParseIntegers {
                 sum += Integer.parseInt(word);
             }
         }
+
+        String forJustWords = " JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations.";
+
+        justWords = forJustWords.replaceAll("\\d+ ", "");
+
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
     }
